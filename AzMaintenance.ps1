cb8d9558b490b2de9ecd02732651de83a39b2476
Ayan <ayanmullick@gmail.com>
2025-07-12 18:28:38 -0500

    Add AzMaintenance and Defender scripts for VM maintenance and security configuration

---
+++ AzMaintenance.ps1
@@ -0,0 +1,15 @@
+﻿Get-AzMaintenanceUpdate -ResourceGroupName '<>-rg' -ProviderName Microsoft.Compute -ResourceType virtualMachines -ResourceName azcrtdccra01
+<#
+MaintenanceScope    : OSImage
+ImpactType          : Restart
+Status              : Pending
+ImpactDurationInSec : 3600
+ResourceId          : /subscriptions/<>/resourcegroups/<>-rg/providers/Microsoft.Compute/virtualMachines/azcrtdccra01
+
+
+MaintenanceScope    : Extension
+ImpactType          : Restart
+Status              : Pending
+ImpactDurationInSec : 3600
+ResourceId          : /subscriptions/<>/resourcegroups/<>-rg/providers/Microsoft.Compute/virtualMachines/azcrtdccra01
+#>
---
+++ Windows/Defender.ps1
@@ -0,0 +1,18 @@
+﻿Set-MpPreference -DisableBehaviorMonitoring $false
+Set-MpPreference -ScanScheduleDay 1
+
+Set-MpPreference -ScanScheduleTime 22:00:00
+
+
+#Gets Vm's in a resource group, enables real time protection and configures AV scanning for 5:00 AM on Sundays
+(Get-AzureRmVM -ResourceGroupName NLGSUSUTMRASRG2).Name|
+                ForEach-Object {Invoke-Command -ComputerName $PSItem -ScriptBlock {Set-MpPreference -DisableBehaviorMonitoring $false -Verbose;Set-MpPreference -ScanScheduleDay 1 -Verbose;Set-MpPreference -ScanScheduleTime 05:00:00 -Verbose}
+                                }
+
+
+Invoke-Command -ComputerName NLGDVJAMVM1 -ScriptBlock {Get-MpPreference}
+
+Get-WmiObject -Namespace "root\Microsoft\SecurityClient" AntimalwareHealthStatus
+
+
+mpcmdrun -getfiles  #Gets all the defender related logs
