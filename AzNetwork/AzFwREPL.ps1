9cba179eddd94819a9fbaccea8fd25a0c84ef6e6
Ayan <ayanmullick@gmail.com>
2026-05-18 17:09:42 -0500

    .

--- AzNetwork/AzFwREPL.ps1
+++ AzNetwork/AzFwREPL.ps1
@@ -1,5 +1,3 @@
-
-
 #region Get the Azure Firewall policy rule associated with a specific ip group
 $resourceGroupName, $firewallPolicyName, $ipGroupName = '<rg-vwan>', 'vhub-prem-pol<>', 'ipgroup<Name>'
 
@@ -62,3 +60,48 @@ $MatchingGroups | Select-Object Name, ResourceGroupName, Location, IpAddresses |
 
 
 
+
+#region Get the Azure Firewall policy rule associated with a specific ip group
+$resourceGroupName, $firewallPolicyName, $ipGroupName = '<rg-vwan>', 'vhub-prem-pol<>', 'ipgroup<Name>' # 'ipgroup<>-01' #'ipgroup-avd<>-01' #'ipgroup-<>-01'
+
+# Get the IP group
+#$ipGroup = Get-AzIpGroup -ResourceGroupName $resourceGroupName -Name $ipGroupName
+#Get-AzIpGroup -Name *-d-*   #Get Dev IP groups
+# Get the firewall policy rule collection group
+$ruleCollectionGroup = Get-AzFirewallPolicyRuleCollectionGroup -ResourceGroupName $resourceGroupName -AzureFirewallPolicyName $firewallPolicyName -Name DefaultNetworkRuleCollectionGroup
+
+#$ruleCollectionGroup.Properties.RuleCollection
+#$ruleCollectionGroup.Properties.RuleCollection|select Name,Priority,@{n='Action';e={$_.action.type}}, RuleCollectionType  #To see the rule collection
+
+$Rules = $ruleCollectionGroup.Properties.RuleCollection.Rules
+
+
+# Filter rules where SourceIpGroups or DestinationIpGroups contain ipgroup-t-c-01
+#$Rules | Where-Object {$_.SourceIpGroups -match $ipGroupName}
+#$Rules | Where-Object {$_.SourceIpGroups -like "*$ipGroupName*"}
+#$Rules | Where-Object {$_.SourceIpGroups -like "*$ipGroupName*" -or $_.DestinationIpGroups -like "*$ipGroupName*"}|FT
+
+
+$Rules.Where({$_.SourceIpGroups -like "*$ipGroupName*" -or $_.DestinationIpGroups -like "*$ipGroupName*"}) |
+Select Name, @{N='SourceIpGroups'; E={($_.SourceIpGroups -split '/')[-1]}}, @{N='DestinationIpGroups'; E={($_.DestinationIpGroups -split '/')[-1]}}, Protocols, RuleType |
+FT -AutoSize -Wrap
+
+
+#Network rules with '-d-' in name ie. Dev environment
+$Rules|? Name -Match '-d-'|
+    Select Name, @{N='SourceIpGroups'; E={($_.SourceIpGroups -split '/')[-1]}}, @{N='DestinationIpGroups'; E={($_.DestinationIpGroups -split '/')[-1]}}, Protocols, RuleType |
+    FT -AutoSize -Wrap
+
+
+#Application rules
+$ipGroupName = 'ipgroup-avd-<>-01'
+
+$ApplicationRuleCollectionGroup = Get-AzFirewallPolicyRuleCollectionGroup -ResourceGroupName $resourceGroupName -AzureFirewallPolicyName $firewallPolicyName -Name DefaultApplicationRuleCollectionGroup
+$ApplicationRules = $ApplicationRuleCollectionGroup.Properties.RuleCollection.Rules
+$apprule = $ApplicationRules.Where({$_.SourceIpGroups -like "*$ipGroupName*" -or $_.DestinationIpGroups -like "*$ipGroupName*"}) 
+
+$apprule|select -Property Name, RuleType, @{N= 'Port' ; E= { $_.Protocols.Port }},"SourceIpGroups",SourceAddresses, TargetUrls, TargetFqdns 
+
+#'ipgroup-<>-01' IP group has internet access thru 'app-rule-<>-01'
+
+#endregion
